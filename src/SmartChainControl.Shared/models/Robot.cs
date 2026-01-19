3f363135f72d9eb1f0267583e6c8bab57c873435
Szénássy Ákos <231650753+akos050607@users.noreply.github.com>
2026-01-19 20:05:05 +0100

    structure initialized

---
+++ src/SmartChainControl.Shared/models/Robot.cs
@@ -0,0 +1,21 @@
+namespace SmartChainControl.Shared.Models
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public float TargetX { get; set; }
+        public float TargetY { get; set; }
+
+        public float Rotation { get; set; }
+
+        public string State { get; set; } = "Idle";
+
+        public string ColorHex { get; set; } = "#00FF00";
+
+        public bool HasCargo { get; set; }
+    }
+}
